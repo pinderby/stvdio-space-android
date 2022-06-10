dbbf0c6178214bd7ab72574cb25e3b9869fdcc5b
David Mangold <pinderby@gmail.com>
2022-06-09 22:13:12 -0700

    Scaled logo on login/signup, bypassed analytics interstitial

--- vector/src/debug/java/im/vector/app/config/AnalyticsConfig.kt
+++ vector/src/debug/java/im/vector/app/config/AnalyticsConfig.kt
@@ -20,7 +20,8 @@ import im.vector.app.BuildConfig
 import im.vector.app.features.analytics.AnalyticsConfig
 
 val analyticsConfig: AnalyticsConfig = object : AnalyticsConfig {
-    override val isEnabled = BuildConfig.APPLICATION_ID == "im.vector.app.debug"
+//    override val isEnabled = BuildConfig.APPLICATION_ID == "im.vector.app.debug"
+    override val isEnabled = false // --DTM-- Updated
     override val postHogHost = "https://posthog.element.dev"
     override val postHogApiKey = "phc_VtA1L35nw3aeAtHIx1ayrGdzGkss7k1xINeXcoIQzXN"
     override val policyLink = "https://element.io/cookie-policy"
